3e73ae3aef336285c08462aee06b4a7c91611de8
ahmad al fajri <116804256+inven22@users.noreply.github.com>
2024-08-12 14:59:40 +0700

    Update __init__.py

--- api_gate/controllers/__init__.py
+++ api_gate/controllers/__init__.py
@@ -1,3 +1,3 @@
 from . import student_controller, jurnal_controller, auth_controller,tugas_controller, elearning_controller
 from . import parent_controller, perpustakaan_controller, jadwal_controller, raport_controller
-from . import activity_controller, invoice_controller, calender_controller, donasi_controller
+from . import activity_controller, invoice_controller, calender_controller, donasi_controller,wakaf_controller
